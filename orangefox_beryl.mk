5816d922646c638729eca36584b573e263fa748b
sairajkandkar0110-design <sairajkandkar0110@gmail.com>
2026-03-19 01:03:54 +0530

    Update and rename omni_beryl.mk to orangefox_beryl.mk

---
+++ orangefox_beryl.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from beryl device
+$(call inherit-product, device/xiaomi/beryl/device.mk)
+
+# Inherit OrangeFox configuration (CRITICAL: Change from omni to otter)
+$(call inherit-product, vendor/otter/config/common.mk)
+
+# Device identifier
+PRODUCT_DEVICE := beryl
+PRODUCT_NAME := orangefox_beryl
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO M7 Pro
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="beryl-user 15 VKW2403.001 release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/beryl/beryl:15/VKW2403.001/release-keys
